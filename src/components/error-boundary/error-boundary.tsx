84fde26316ef143aa69669717746d094183c9565
Danil Nikulitsa <danilnikulitsa@yandex.ru>
2025-11-22 08:17:50 +0300

    fix

--- src/components/error-boundary/error-boundary.tsx
+++ src/components/error-boundary/error-boundary.tsx
@@ -21,7 +21,6 @@ export class ErrorBoundary extends Component<Props, State> {
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error('ErrorBoundary caught an error:', error, errorInfo);
-    // Здесь можно отправить ошибку в Sentry, LogRocket, или просто в консоль
   }
 
   render() {
